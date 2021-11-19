9d70ec71177dad939d46c9f5bf0d7410de651611
Stephen McCabe <stephentmccabe@gmail.com>
2021-11-19 13:00:10 -0800

    added dotenv

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+
+
+
+
+const asyncWrapper = (func) => {
+  return (req, res, next) => {
+    func(req, res, next).catch(next);
+  };
+};
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", async (msg) => {
+  if (msg.content ==='test') {
+    msg.reply('test')
+  }
+});
+
+client.login(process.env.CLIENT_TOKEN);
