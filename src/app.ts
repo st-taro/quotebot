3112238dc9dc22e42998739141950e18f0871763
Stephen McCabe <stephentmccabe@gmail.com>
2021-11-23 16:45:05 -0800

    sidelined crawler to focus on more basic crud

--- src/app.ts
+++ src/app.ts
@@ -10,17 +10,39 @@ import {
 } from '@discordjs/voice';
 import { createDiscordJSAdapter } from './adapter';
 
-const { token } = require('../config.json');
-
+const mongoose = require('mongoose');
+const path = require('path');
+const cfg = require('../config.json');
+const Quote = require('./models/quote')
+const { quoteSchema } = require('./schemas')
 
 const voiceHelpers = require('./utils/discordVoiceHelpers');
 
-
 /**
- * Main code
- * =========
- * Here we will implement the helper functions that we have defined above.
+ * Mongoose connection
  */
+mongoose.connect('mongodb://' + cfg.host + ':' + cfg.port + '/dotabot', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const addQuote = async (quote: string, url: string) => {
+  let stripped = quote.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+  const newQuote = new Quote({
+    quote: quote,
+    url: url,
+    stripped: stripped
+  })
+  await newQuote.save();
+}
+
+
 
 const client = new Client({
   intents: [
@@ -30,7 +52,7 @@ const client = new Client({
   ],
 });
 
-void client.login(token);
+void client.login(cfg.token);
 
 client.on('ready', async () => {
   console.log('Discord.js client is ready!');
@@ -52,6 +74,14 @@ client.on('ready', async () => {
 client.on('messageCreate', async (message) => {
   if (!message.guild) return;
 
+
+  // Add a message
+  if (message.content.substring(0, 4) === '-add ') {
+
+  }
+
+
+  // Play a message
   if (message.content === '-join') {
     const channel = message.member?.voice.channel;
 
